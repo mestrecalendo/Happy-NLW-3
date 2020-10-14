87a461f635fee3fc24a20a720cf53cfd90c52eca
mestrecalendo <57951365+mestrecalendo@users.noreply.github.com>
2020-10-14 11:39:03 -0400

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Happy NLW#3
  Projeto Happy desenvolvido na Next Level Week 3
+<img src="./imgp/nlw.jpg"/>
--- public/script/container_orfanatos.js
+++ public/script/container_orfanatos.js
@@ -1,11 +1,8 @@
-const map = L.map("mapid").setView([51.505, -0.09], 13);
+const map = L.map("mapid").setView([-8.7654034,-63.910287], 14);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-L.marker([51.5, -0.09])
+L.marker([-8.7654034,-63.910287])
   .addTo(map)
   .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
   .openPopup();
